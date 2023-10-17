508506cb5e9df35296bc85e8cd8844279277fd43
varvara-riddec <123941803+varvara-riddec@users.noreply.github.com>
2023-10-17 14:32:29 +0300

    feat(XG-769): step-two base added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
+const currencies = require("../utils/currencies");
+
 const router = express.Router();
 
 // eslint-disable-next-line no-unused-vars
-router.get("/", (req, res, next) => res.render("step-one"));
+router.get("/", (req, res, next) => res.render("step-one", {
+  currencies,
+}));
+
+// eslint-disable-next-line no-unused-vars
+router.get("/step-two", (req, res, next) => res.render("step-two", {
+  currencies,
+}));
 
 router.use("/not-found", (_req, res) => res.render("error", {
   status: "404",
